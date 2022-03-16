4376be1832203e78c39b5ff320c559f4afbf5c7e
Samuel Buzas <buzassg@mail.uc.edu>
2022-03-15 22:28:01 -0400

    inital commit

---
+++ src/main/java/com/example/imagebuildcaching/ImageBuildCachingApplication.java
@@ -0,0 +1,13 @@
+package com.example.imagebuildcaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageBuildCachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageBuildCachingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/imagebuildcaching/ImageBuildCachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imagebuildcaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageBuildCachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
